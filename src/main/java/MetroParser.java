43f52922d01c722c7bb9cc58592f40411fba3b54
ivan fomkin <ivan-fomkin@yandex.ru>
2020-01-25 19:10:12 +0300

    some function rewrite

--- src/main/java/MetroParser.java
+++ src/main/java/MetroParser.java
@@ -4,24 +4,51 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.net.URLDecoder;
-import java.util.*;
-import java.util.List;
 
 public class MetroParser {
     private StationIndex metro;
-    private final String linkToWiki = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D0%B9_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_%D0%BC%D0%B5%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B5%D0%BD%D0%B0";
+    private final String LINK_TO_WIKI = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D0%B9_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_%D0%BC%D0%B5%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B5%D0%BD%D0%B0";
 
     public StationIndex getStationIndex() throws IOException {
         metro = new StationIndex();
-//        metro = parseStationIndexFromWiki();
-//        parseConnectionFromWiki(metro);
-        parseLineFromWiki(metro);
-        parseStationFromWiki(metro);
+        parseLines(metro);
+        parseStations(metro);
         return metro;
     }
 
-    private String getColorFromTdStyle(String styleAttribute) { //Метод, который парсит цвет из атрибута style у td
+    private Elements getRows() throws IOException {
+        //Получаем стройки таблиц, которые содержат инфо о станции в википедии
+        Document document = Jsoup.connect(LINK_TO_WIKI).maxBodySize(0).get(); //снимаем ограничение по размеру
+        Elements tableLine = document.select("table[class=standard sortable]"); //Получаем все таблицы с классом standard sortable, именно в них лежат станции
+        return tableLine.select("tbody").select("tr"); //Получаем все строки этих таблиц и возвращаем их
+    }
+
+    private void parseLines(StationIndex myMetro) throws IOException {
+        Elements rows = getRows();
+
+        for (Element element : rows) { //Перебираем все строки, содержащих инфо о станциях
+            Elements cells = element.select("td"); //Получаем ячейки каждой строки
+            //Таблица, где 7 ячеек - монорельс или МЦК, 8 - метро
+            if (cells.size() == 7 || cells.size() == 8) {
+                String lineNumber = cells.select("span.sortkey").first().text(); //Получаем номер линии
+                String lineName = cells.get(0).select("img").attr("alt"); //Название линии
+                lineName = lineName.replaceAll(" линия", ""); //Удаляем слово "линия"
+                String lineColor;
+                //Т.к. у 11A цвет не прописан, придётся задать его вручную
+                if (lineNumber.equals("011А")) {
+                    lineColor = "#82C0C0";
+                } else { //остальные парсим
+                    lineColor = parseColor(cells.select("td[style]").attr("style"));
+                }
+                Line line = new Line(lineNumber, lineName, lineColor);
+                myMetro.addLine(line);
+            }
+        }
+    }
+
+    private String parseColor(String styleAttribute) {
+        //Метод, который парсит цвет из атрибута style у td
+        //Цвет начинается с #, значит нам надо его найти, и взять 6 символов после него
         String color;
         int firstSharpSymbol = 0; // Будем хранить первую #тут
         for (int i = 0; i < styleAttribute.length(); i++) { //Пробегамся по строке, ищем первый #
@@ -54,58 +81,20 @@ public class MetroParser {
         return stationName;
     }
 
-    private Elements getTableLineFromWiki() throws IOException { //Получаем стройки таблиц, которые содержат инфо о станции в википедии
-        Document document = Jsoup.connect(linkToWiki).maxBodySize(0).get(); //снимаем ограничение по размеру
-        Elements tableLine = document.select("table[class=standard sortable]"); //Получаем все таблицы с классом standard sortable, именно в них лежат станции
-        return tableLine.select("tbody").select("tr"); //Получаем все строки этих таблиц и ретёрним их
-    }
 
-    private void parseLineFromWiki(StationIndex stationIndexForParsing) throws IOException {
-        Elements tableLine = getTableLineFromWiki();
+    private void parseStations(StationIndex myMetro) throws IOException {
+        Elements rows = getRows();
 
-        for (Element element : tableLine) { //Перебираем все строки таблиц, содержащих инфо о станциях
-            Elements cells = element.select("td"); //Получаем элементы, содержащие ячейки каждой линии
-            //Если ячеек 7 - это монорельс или Московское центральное кольцо
-            //Если ячеек 8 - это обычное метро
-
+        for (Element element : rows) { //Перебираем все строки, содержащих инфо о станциях
+            Elements cells = element.select("td"); //Получаем ячейки каждой строки
+            //Таблица, где 7 ячеек - монорельс или МЦК, 8 - метро
             if (cells.size() == 7 || cells.size() == 8) { //Если в строке 7 или 8 ячеек то парсим инфу оттуда
                 String lineNumber = cells.select("span.sortkey").first().text(); //Получаем номер линии
-                String lineName = cells.get(0).select("img").attr("alt"); //Название линии
-                lineName = lineName.replaceAll(" линия", ""); //Удаляем слово "линия"
-                String lineColor;
-                //Т.к. у 11A цвет не прописан, придётся задать его вручную
-                if (lineNumber.equals("011А")) {
-                    lineColor = "#82C0C0";
-                } else { //остальные парсим
-                    lineColor = getColorFromTdStyle(cells.select("td[style]").attr("style"));
-                }
-                Line currentLine = new Line(lineNumber, lineName, lineColor);
-
-
-                stationIndexForParsing.addLine(currentLine);
-            }
-        }
-    }
-
-    private void parseStationFromWiki(StationIndex stationIndexForParsing) throws IOException {
-        Elements tableLine = getTableLineFromWiki();
-
-        for (Element element : tableLine) { //Перебираем все строки таблиц, содержащих инфо о станциях
-            Elements cells = element.select("td"); //Получаем элементы, содержащие ячейки каждой линии
-            //Если ячеек 7 - это монорельс или Московское центральное кольцо
-            //Если ячеек 8 - это обычное метро
-
-            if (cells.size() == 7 || cells.size() == 8) { //Если в строке 7 или 8 ячеек то парсим инфу оттуда
-                String lineNumber = cells.select("span.sortkey").first().text(); //Получаем номер линии
-                Line currentLine = stationIndexForParsing.getLine(lineNumber);
-                System.out.println("Линия " + currentLine.getName() + " получена");
+                Line line = myMetro.getLine(lineNumber);
                 String stationName = cells.get(1).select("a").text().replaceAll("[0-9]", "");
-                Station currentStation = new Station(stationName, currentLine);
-                System.out.println("Станция " + currentStation.getName() + " на линии " + currentLine.getName() + " создана");
-                stationIndexForParsing.addStation(currentStation);
-                System.out.println("Станция добавлено в метро");
-                currentLine.addStation(currentStation);
-                System.out.println("Станция добавлена на линию");
+                Station station = new Station(stationName, line);
+                myMetro.addStation(station);
+                line.addStation(station);
             }
         }
     }
